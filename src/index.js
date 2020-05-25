8c4ac27a9b81408eac2aa1875c54c614c42a737a
Myles Young <myles.young@mailchimp.com>
2020-05-25 12:06:09 -0400

    middleware intercepts incoming dispatch & returns a function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './data-store/reducers/rootReducer';
+import thunk from 'redux-thunk';
 
 import "./Components/index.css";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
