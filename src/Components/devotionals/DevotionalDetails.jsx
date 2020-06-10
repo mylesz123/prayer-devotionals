b6f57c316d4694f45c8970ee977d94e70aec8afb
Myles Young <myles.young@mailchimp.com>
2020-06-09 22:14:45 -0400

    connect to firebase and show selected devotional details

--- src/Components/devotionals/DevotionalDetails.jsx
+++ src/Components/devotionals/DevotionalDetails.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
 
-export default function DevotionalDetails(props) {
-    const id = props.match.params.id;
+const mapStateToProps = (state, componentProps) => {
+    const id = componentProps.match.params.id;
+    const slicedId = id.slice(1);
+    const devotionals = state.firestore.data.devotionals;
+    const devotional = devotionals && devotionals[slicedId];
 
+    return {
+        devotional,
+    }
+}
+
+function DevotionalDetails({ devotional }) {
     return (
-        <div className=" continer section devotional-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Title ID:{id}</span>
-                    <p>Some Content to look at for now</p>
-                </div>
+        <>
+            {devotional 
+                ? <div className=" container section devotional-details">
+                        <div className="card z-depth-0">
+                            <div className="card-content">
+                                <span className="card-title">Title: {devotional.title}</span>
+                                <p>{devotional.content}</p>
+                            </div>
 
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted By: ME</div>
-                    <div>13, May, 10:00 PM</div>
-                </div>
+                            <div className="card-action grey lighten-4 grey-text">
+                            <div>Posted By: {devotional.authorFirstName}</div>
+                                <div>{devotional.time}</div>
+                            </div>
 
-            </div>
-        </div>
+                        </div>
+                    </div>
+                : <div className="container center">Loading ... </div>
+            }
+        </>
     )
 }
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: "devotionals" }
+    ])
+)(DevotionalDetails);
