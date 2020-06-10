193c6b48f811ab0ff1a5ca5fda46920a6a794b81
Myles Young <myles.young@mailchimp.com>
2020-06-09 22:11:03 -0400

    link to devotionals from dashboard

--- src/Components/devotionals/DevotionalsList.jsx
+++ src/Components/devotionals/DevotionalsList.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import DevotionalSummary from './DevotionalSummary';
+import { Link } from 'react-router-dom';
 
 export default function DevotionalsList({ devotionals }) {
     return (
         <div className=" devotional-list section">
             {devotionals && devotionals.map(devotional => (
-                <DevotionalSummary 
-                    key={devotional.id} 
-                    {...devotional}
-                />
+                <Link key={devotional.id} to={`/devotional/:${devotional.id}`} >
+                    <DevotionalSummary  
+                        {...devotional}
+                    />
+                </Link>
             ))}
         </div>
     )
