63eb8871d87776da8b07e401ca70fd1b4b93af54
Myles Young <myles.young@mailchimp.com>
2020-06-14 22:00:55 -0400

    connect signed out method to components

--- src/Components/layout/SignedinLinks.jsx
+++ src/Components/layout/SignedinLinks.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOutAction } from '../../data-store/actions/authenticationActions';
 
-export default function SignedinLinks() {
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOutAction())
+    }
+}
+
+function SignedinLinks({ signOut }) {
     return (
         <ul className="right">
             <li>
                 <NavLink to="/create-devotional">New Devotional</NavLink>
             </li>
             <li>
-                <NavLink to="/">Log Out</NavLink>
+                <a onClick={signOut} href="/">Log Out</a>
             </li>
             <li>
                 <NavLink to="/" className="btn btn-floating pink lighten-1">MY</NavLink>
@@ -16,3 +24,5 @@ export default function SignedinLinks() {
         </ul>
     )
 }
+
+export default connect(null, mapDispatchToProps)(SignedinLinks)
