553e7a7742ad7721a88b6d5e6d9e42b25d9ec299
Myles Young <myles.young@mailchimp.com>
2020-07-11 17:44:22 -0400

    add signup actions

--- src/data-store/actions/authenticationActions.js
+++ src/data-store/actions/authenticationActions.js
@@ -16,6 +16,33 @@ export const loginAction = (state = initState) => {
     }
 }
 
+export const signUpAction = (state) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        firebase.auth().createUserWithEmailAndPassword(
+            state.email,
+            state.password
+        )
+        .then((response) => {
+            return firestore.collection('users')
+                .doc(response.user.uid)
+                .set({
+                    firstName: state.firstName,
+                    lastName: state.lastName,
+                    initials: state.firstName[0] + state.lastName[0],
+                });
+        })
+        .then(() => {
+            dispatch({ type: "SIGNUP_SUCCESS" });
+        })
+        .catch((error) => {
+            dispatch({ type: "SIGNUP_ERROR", error });
+        })
+    }
+}
+
 export const signOutAction = () => {
     return (dispatch, getState, {getFirebase}) => {
         const firebase = getFirebase();
--- src/data-store/reducers/authenticationReducer.js
+++ src/data-store/reducers/authenticationReducer.js
@@ -14,7 +14,21 @@ const authenticationReducer = (state = initState, action) => {
             console.log("Login Failed");
             return { 
                 ...state, 
-                authError: "Login Failed, could not find username or password"
+                authError: "Login Failed, could not find username or password: " + action.error.message
+            };
+
+        case "SIGNUP_SUCCESS":
+            console.log("Signup Successful");
+            return {
+                ...state,
+                authError: null
+            };
+
+        case "SIGNUP_ERROR":
+            console.log("Signup Failed");
+            return { 
+                ...state, 
+                authError: "Signup Failed, could not create a user: " + action.error.message
             };
 
         case "SIGNOUT_SUCCESS":
@@ -22,7 +36,7 @@ const authenticationReducer = (state = initState, action) => {
             return state;
 
         case "SIGNOUT_ERROR":
-            console.log("Sign out Failed");
+            console.log("Sign out Failed: " + action.error.message);
             return state;
     
         default:
