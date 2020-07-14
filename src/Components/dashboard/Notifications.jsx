2da8588e8576869410c8afc4ce9f69ab427e5c44
Myles Young <myles.young@mailchimp.com>
2020-07-13 20:40:34 -0400

    edit notifications component

--- src/Components/dashboard/Notifications.jsx
+++ src/Components/dashboard/Notifications.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Notifications() {
     return (
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title center-align">Notifications</span>
+                    <ul className="notification">
+                        <li>WTF</li>
+                        <li>WTF</li>
+                        <li>WTF</li>
+                        <li>WTF</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
