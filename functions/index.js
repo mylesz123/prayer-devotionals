61fde7db6560e7931d0cd7025635a80340d4591d
Myles Young <myles.young@mailchimp.com>
2020-07-18 20:32:06 -0400

    add firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Jesus is King! 🔥");
 });
+
+// create notification document in firebase
+const createNotification = (notification) => {
+    return admin.firestore()
+        .collection('notifications')
+        .add(notification)
+        .then(document => console.log("Notification created!", {document: document}))
+}
+
+exports.devotionalCreatedNotification = functions.firestore
+    .document('devotionals/{devotionalID}')
+    .onCreate(document => {
+        const devotional = document.data();
+        const notification = {
+            user: `${devotional.authorFirstName} ${devotional.authorLastName}`,
+            content: 'added a new devotional',
+            time: admin.firestore.FieldValue.serverTimestamp(),
+        }
+
+        return createNotification(notification);
+    });
+
+exports.userJoinedNotification = functions.firestore
+    .document('users/{userID}')
+    .onCreate(document => {
+        const user = document.data();
+        const notification = {
+            user: `${user.firstName} ${user.lastName}`,
+            content: "joined the team!",
+            time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+    });
