f1b295c71315eaa42d6f0d15413b9eee19e37995
Myles Young <myles.young@mailchimp.com>
2020-07-23 22:43:19 -0400

    wire up EditDevotional -- should abstract button into its own file

--- src/Components/devotionals/DevotionalDetails.jsx
+++ src/Components/devotionals/DevotionalDetails.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EditDevotional from './EditDevotional';
+
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -11,37 +13,77 @@ const mapStateToProps = (state, componentProps) => {
     const devotional = devotionals && devotionals[id];
 
     return {
+        devotionalID: id,
         devotional,
         isLoggedIn: state.firebase.auth.uid,
     }
 }
 
-function DevotionalDetails({ devotional, isLoggedIn }) {
-    if (isLoggedIn) {
+function Button({ 
+    text, 
+    handleClick = null
+}) {
+    const onClick = (e) =>  {
+        e.preventDefault();
+    }
+
+    const styles = {
+        edit: "waves-effect waves-light btn purple accent-3 right",
+    }
+    return (
+        <button 
+            className={styles.edit}
+            onClick={handleClick || onClick}
+        > 
+            {text}
+        </button>
+    )
+}
+
+function DevotionalDetails({ devotionalID, devotional, isLoggedIn }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEdit = (bool) => {
+        setIsEditing(bool);
+    }
+
+    if (!isLoggedIn) {
+        return (<Redirect to={'/login'} />)
+    }
+
+    //should also check if the id of the current user matches the authorId in the devotional
+    if (isEditing) {
         return (
-            <>
-                {devotional
-                    ? <div className=" container section devotional-details">
-                        <div className="card z-depth-0">
-                            <div className="card-content">
-                                <span className="card-title">Title: {devotional.title}</span>
-                                <p>{devotional.content}</p>
-                            </div>
+            <EditDevotional
+                id={devotionalID}
+                devotional={devotional} 
+                setIsEditing={setIsEditing} 
+            />
+        )
+    }
 
-                            <div className="card-action grey lighten-4 grey-text">
-                                <div>Posted By: {devotional.authorFirstName}</div>
-                                <div>{devotional.time}</div>
-                            </div>
+    return (
+        <>
+            {devotional
+                ? <div className=" container section devotional-details">
+                    <div className="card z-depth-0">
+                        <div className="card-content">
+                            <span className="card-title">Title: {devotional.title}</span>
+                            <p>{devotional.content}</p>
+                        </div>
 
+                        <div className="card-action grey lighten-4 grey-text">
+                            <div>Posted By: {devotional.authorFirstName}</div>
+                            <div>{devotional.time}</div>
                         </div>
                     </div>
-                    : <div className="container center">Loading ... </div>
-                }
-            </>
-        )
-    } else {
-        return (<Redirect to={'/login'}/>)
-    }
+
+                    <Button text="Edit" handleClick={() => handleEdit(true)}/>
+                </div>
+                : <div className="container center">Loading ... </div>
+            }
+        </>
+    )
 }
 
 export default compose(
